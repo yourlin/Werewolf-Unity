5eaaa556825c5177fbfbed571212cc584a06b045
LinYe <31615967@qq.com>
2024-04-08 14:40:20 +0800

    tons of update

--- Assets/Script/Game/GameApp.cs
+++ Assets/Script/Game/GameApp.cs
@@ -7,6 +7,7 @@ using TMPro;
 using UnityEngine.UI;
 using Unity.VisualScripting;
 using static System.Net.Mime.MediaTypeNames;
+using UnityEngine.Playables;
 //using UnityEngine.Networking;
 //using Unity.Plastic.Newtonsoft.Json;
 
@@ -20,22 +21,33 @@ public class GameApp : UnitySingleton<GameApp> {
     public UnityEngine.UI.Image playerImg;
 	public List<Sprite> PlayerImageList;
     private bool isMsgHandling = false;
+	public TMP_Text mainRound;
+	public GameObject Roundboard;
+	private int currentRound = 0;
 
-    public bool IsRunning { get => isRunning; set => isRunning = value; }
+	public bool IsRunning { get => isRunning; set => isRunning = value; }
 	public Queue<PlayerMessage> GameMessages { get => gameMessages; set => gameMessages = value; }
-
+	public Dictionary<int, GameObject> dictPlayerObjects= new Dictionary<int, GameObject>();
 
 	/// <summary>
 	/// 初始化
 	/// </summary>
 	public void InitGame()
 	{
-		
+		var players = GameObject.FindGameObjectsWithTag ("Player");
+		foreach (var obj in players) {
+			Player player = obj.GetComponent<Player>();
+			dictPlayerObjects.Add(player.Id, obj);
+			// todo 动态生成Clip？
+			//player.GetComponent<Animation>().GetClip("Player_Idel_Left")
+		}
+
+		// get start
+		StartCoroutine (run ());
 	}
 
 	void Start () {
-		// get start
-		StartCoroutine (run ());
+		InitGame ();
 	}
 
 	/// <summary>
@@ -77,13 +89,14 @@ public class GameApp : UnitySingleton<GameApp> {
 		Debug.Log ("Request a message");
 
 		// mock up 等待1秒
-		int id = UnityEngine.Random.Range (1, 12);
+		int id = UnityEngine.Random.Range (1, GameSetting.PlayerNum);
 		PlayerMessage msg = new PlayerMessage ();
         msg.Round = id;
-        msg.PlayerName = $"Player {id}";
+        msg.PlayerName = $"Player{id}";
 		msg.PlayerId = id;
-		msg.Type = PlayerMessageType.PlayerMessage;
-		msg.Message = $"This is {msg.PlayerName} speaking, test test test test test test test test test test test test test test test test test test testThis is {msg.PlayerName} speaking,This is {msg.PlayerName} speaking,This is {msg.PlayerName} speaking,This is {msg.PlayerName} speaking,This is {msg.PlayerName} speaking,This is {msg.PlayerName} speaking,This is {msg.PlayerName} speaking,This is {msg.PlayerName} speaking,This is {msg.PlayerName} speaking,This is {msg.PlayerName} speaking,This is {msg.PlayerName} speaking,This is {msg.PlayerName} speaking,This is {msg.PlayerName} speaking,This is {msg.PlayerName} speaking,This is {msg.PlayerName} speaking,This is {msg.PlayerName} speaking,This is {msg.PlayerName} speaking,This is {msg.PlayerName} speaking, ";
+		msg.TargetId = id - 1 > 0 ? id - 1 : id+1;
+		msg.Type = PlayerMessageType.WolfMessage;
+		msg.Message = $"This is {msg.PlayerName} speaking, test test test 中文测试 test test test test test test test中文测试。This is {msg.PlayerName} speaking 中文测试,This is {msg.PlayerName} speaking,This is {msg.PlayerName} speaking,This is {msg.PlayerName} speaking,This is {msg.PlayerName} speaking,This is {msg.PlayerName} speaking,This is {msg.PlayerName} speaking,This is {msg.PlayerName} speaking,This is {msg.PlayerName} speaking,This is {msg.PlayerName} speaking,This is {msg.PlayerName} speaking,This is {msg.PlayerName} speaking,This is {msg.PlayerName} speaking,This is {msg.PlayerName} speaking, ";
 		gameMessages.Enqueue (msg);
 
 		/*
@@ -112,8 +125,14 @@ public class GameApp : UnitySingleton<GameApp> {
 				continue;
 			}
 			isMsgHandling = true;
-			Debug.Log($"GameMessages.Count = {GameMessages.Count}");
 			PlayerMessage msg = GameMessages.Dequeue();
+			
+			if (IsRoundChanged (msg.Round)) {
+				mainRound.text = $"Round {msg.Round}";
+				StartCoroutine (ShowRoundBoard (msg.Round));
+				this.currentRound = msg.Round;
+			}
+			
 			switch (msg.Type) {
 			case PlayerMessageType.PlayerMessage:
 				onPlayerMessage (msg);
@@ -131,8 +150,6 @@ public class GameApp : UnitySingleton<GameApp> {
 				onGameConclusion (msg);
 				break;
 			}
-
-            isMsgHandling = false;
         }
     }
 
@@ -148,39 +165,78 @@ public class GameApp : UnitySingleton<GameApp> {
 
 	private void onProphetMessage(PlayerMessage msg)
 	{
-		throw new NotImplementedException();
+		Debug.Log ($"{msg.PlayerName} 预言家发言");
+		var player = dictPlayerObjects [msg.PlayerId].GetComponent<Player> ();
+		roundText.text = $"Round {msg.Round} - Wolf";
+		playerName.text = $"{msg.PlayerName}(Wolf)";
+		playerImg.sprite = PlayerImageList [msg.PlayerId % GameSetting.PlayerNum];
+
+		StartCoroutine (TypeText (player, playerMessage, msg.Message, 0.01f));
+
+		// 获取被杀死的对象
+		var targetPlayer = dictPlayerObjects [msg.TargetId].GetComponent<Player> ();
+		targetPlayer.State = PlayerState.Dead;
+		Debug.Log ($"Player {targetPlayer.Id} was killed");
 	}
 
-	private void onWolfMessage(PlayerMessage msg)
+    void onWolfMessage(PlayerMessage msg)
 	{
-		throw new NotImplementedException();
+		Debug.Log ($"{msg.PlayerName} 狼人发言");
+		var player = dictPlayerObjects [msg.PlayerId].GetComponent<Player> ();
+		roundText.text = $"Round {msg.Round} - Wolf";
+		playerName.text = $"{msg.PlayerName}(Wolf)";
+		playerImg.sprite = PlayerImageList [msg.PlayerId % GameSetting.PlayerNum];
+
+		StartCoroutine (TypeText (player, playerMessage, msg.Message, 0.01f));
+
+		// 获取被杀死的对象
+		var targetPlayer = dictPlayerObjects [msg.TargetId].GetComponent<Player> ();
+		targetPlayer.State = PlayerState.Dead;
+		Debug.Log ($"Player {targetPlayer.Id} was killed");
 	}
 
 	void onPlayerMessage (PlayerMessage msg) {
-        // if the round number changed, show the animation
-        // wait for the animation
-        // find player by id
-        // show message box, text word by word animation and prohaibit
-        // wait for the animation
+		// if the round number changed, show the animation
+		// wait for the animation
+		// find player by id
+		// show message box, text word by word animation and prohaibit
+		// wait for the animation
 
+		var player = dictPlayerObjects [msg.PlayerId].GetComponent<Player> ();
 		roundText.text = $"Round { msg.Round}";
         playerName.text =  msg.PlayerName;
         playerImg.sprite = PlayerImageList[msg.PlayerId % GameSetting.PlayerNum];
-		Debug.Log($"{msg.PlayerName} 开始说话");
-        StartCoroutine(TypeText(playerMessage, msg.Message, 0.01f));
-        
+        StartCoroutine(TypeText(player, playerMessage, msg.Message, 0.01f));
     }
 
-    IEnumerator TypeText(TMP_Text tMP_text, string str, float interval)
+    IEnumerator TypeText(Player player, TMP_Text tMP_text, string str, float interval)
     {
-        int i = 0;
+		player.State = PlayerState.Busy;
+		int i = 0;
         while (i <= str.Length)
         {
             tMP_text.text = str.Substring(0, i++);
             yield return new WaitForSeconds(interval);
         }
 
+		// 消息结束的等待
         yield return new WaitForSeconds(GameSetting.MessageEndWaitforSeconds);
-		Debug.Log("等待1s");
-    }
+		yield return isMsgHandling = false;
+		yield return player.State = PlayerState.Idle;
+	}
+
+	bool IsRoundChanged(int round) {
+		return currentRound != round;
+	}
+
+	IEnumerator ShowRoundBoard (int roundNum) {
+		Debug.Log ("播放round动画");
+		var roundText = Roundboard.transform.gameObject.GetComponentInChildren<TMP_Text> ();
+		roundText.text = $"Round {roundNum}";
+		PlayableDirector director = Roundboard.GetComponent<PlayableDirector> ();
+		Roundboard.SetActive (true);
+		director.Play ();
+		yield return new WaitForSeconds (3);
+		Roundboard.SetActive (false);
+	}
 }
--- Assets/Script/Game/Player.cs
+++ Assets/Script/Game/Player.cs
@@ -21,23 +21,34 @@ public enum PlayerGender {
 
 public class Player : MonoBehaviour
 {
-	string playerName;
-	PlayerRole role;
-	PlayerGender gender;
-	PlayerState state;
+	Animator animator;
+	[SerializeField]
+	private int id;
+	[SerializeField]
+	private string playerName;
+	[SerializeField]
+	private PlayerRole role;
+	[SerializeField]
+	private PlayerGender gender;
+	[SerializeField]
+	private PlayerState state;
 
+	public int Id { get => id; set => id = value; }
 	public string PlayerName { get => playerName; set => playerName = value; }
 	public PlayerRole Role { get => role; set => role = value; }
 	public PlayerGender Gender { get => gender; set => gender = value; }
-	public PlayerState State { get => state; set => state = value; }
+	public PlayerState State {
+		get => state; set {
+			state = value;
 
-	private void Start()
-	{
-		
+			animator.SetBool ("Busy", value == PlayerState.Busy);
+			animator.SetBool ("Death", value == PlayerState.Dead);
+			animator.SetBool ("Idle", value == PlayerState.Idle);
+		}
 	}
 
-	private void FixedUpdate()
+	private void Awake()
 	{
-		
+		animator = this.GetComponent<Animator> ();
 	}
 }
