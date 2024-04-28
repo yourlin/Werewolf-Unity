691fd46c86268236750008f790ef766024e29553
MasterLin <31615967@qq.com>
2024-04-28 14:25:43 +0800

    add opening timeline

--- Assets/Script/Game/OpeningCtrl.cs
+++ Assets/Script/Game/OpeningCtrl.cs
@@ -1,12 +1,18 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class OpeningCtrl : MonoBehaviour {
     ScreenLoader screenLoader;
+    public Button StartButton;
+    //public Texture2D NormalCursorTexture;
+    public Texture2D busyCursorTexture;
+    public TMP_Text ErrorMessage;
     // Start is called before the first frame update
     void Awake () {
         screenLoader = GetComponent<ScreenLoader> ();
@@ -29,7 +35,7 @@ public class OpeningCtrl : MonoBehaviour {
 
     public void onQuit () {
 #if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;//�����˳�����
+        UnityEditor.EditorApplication.isPlaying = false;
 #else
         Application.Quit ();
 #endif
@@ -37,17 +43,23 @@ public class OpeningCtrl : MonoBehaviour {
 
 
     IEnumerator StartGame () {
+        ErrorMessage.text = "";
+        StartButton.interactable = false;
+        Cursor.SetCursor (busyCursorTexture, Vector2.zero, CursorMode.Auto);
         UnityWebRequest request = UnityWebRequest.Get (APIUrl.startGame);
         request.timeout = GameSetting.RequestTimeout;
         yield return request.SendWebRequest ();
+        
         if (request.result == UnityWebRequest.Result.Success) {
             Debug.Log ("开始游戏");
-
-            yield return new WaitForSeconds(2);
             yield return screenLoader.LoadScene (1);
         } else {
             // 请求失败,输出错误信息
             Debug.LogError ("Error: " + request.error);
+            ErrorMessage.text = request.error;
+            StartButton.interactable = true;
+            
         }
+        Cursor.SetCursor (null, Vector2.zero, CursorMode.Auto);
     }
 }
